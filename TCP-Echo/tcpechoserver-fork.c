56e4c9a687f4043c93f285081dff57ec6d06b658
Volodymyr Tykhoniuk <volodymyr.tykhoniuk@gmail.com>
2016-04-05 23:30:30 -0400

    For child logic has been added

--- TCP-Echo/tcpechoserver-fork.c
+++ TCP-Echo/tcpechoserver-fork.c
@@ -15,15 +15,18 @@
 // printf, snprintf
 #include <stdio.h>
 
-// bzero
-#include <strings.h>
+// memset
+#include <string.h>
 
-// time_t, cnet
-#include <time.h>
+// getpid
+#include <unistd.h>
 
 #include "unet.h"
 
 #define PORT        7777
+
+void process_client(int clientfd);
+void sig_child(int signo);
 
 int main()
 {
@@ -32,7 +35,7 @@ int main()
     serverfd = Socket(PF_INET, SOCK_STREAM, 0);
 
     struct sockaddr_in servaddr;
-    bzero(&servaddr, sizeof(servaddr));
+    memset(&servaddr, '\0', sizeof(servaddr));
     servaddr.sin_family = AF_INET;
 
     Sock_set_wild((SA *) &servaddr);
@@ -41,16 +44,46 @@ int main()
     Bind(serverfd, (SA *) &servaddr, sizeof(servaddr));
     Listen(serverfd, 5);
 
+    Signal(SIGCHLD, sig_child);
+
     for (;;) {
         struct sockaddr_in cliaddr;
-        char *cliaddrstr;
-
         socklen_t cliaddrlen = sizeof(cliaddr);
-        int clientfd = Accept(serverfd, (SA *) &cliaddr, &cliaddrlen);
+        int clientfd;
 
-        cliaddrstr = Sock_ntop((SA *) &cliaddr);
-        printf("Connection from [%s]\n", cliaddrstr);
-        free(cliaddrstr);
+        /* Since we explicitly set SA_RESTART flag in Signal wraper
+           we don't care about interrupted syscalls (EINTR error)
+           */
+        clientfd = Accept(serverfd, (SA *) &cliaddr, &cliaddrlen);
+
+        /* Though, for systems that don't support SA_RESTART we would need to
+           use the following code instead of Accept wrapper, to correctly
+           process EINTR error */
+        /*
+        if ((clientfd = accept(serverfd, (SA *) &cliaddr, &cliaddrlen)) < 0) {
+            if (errno == EINTR) continue;
+            else err_sys("accept error");
+        }
+        */
+
+        pid_t childpid;
+        if ((childpid = Fork()) == 0) {
+            // That's a child branch
+
+            /* Close the descriptor we don't need anymore */
+            Close(serverfd);
+
+            /* Logging */
+            char *cliaddrstr = Sock_ntop((SA *) &cliaddr);
+            printf("Child [%d]: Connection from [%s]\n", getpid(), cliaddrstr);
+            free(cliaddrstr);
+
+            process_client(clientfd);
+            Close(clientfd);
+
+            printf("Child [%d]: done processing, exiting...\n", getpid());
+            exit(EXIT_SUCCESS);
+        }
 
         Close(clientfd);
     }
@@ -58,4 +91,25 @@ int main()
     Close(serverfd);
 
     exit(EXIT_SUCCESS);
+}
+
+void process_client(int clientfd)
+{
+    char buf[MAXLINE];
+    size_t n;
+
+    while ((n = Readline(clientfd, buf, MAXLINE)) > 0)
+        Writen(clientfd, buf, n);
+}
+
+void sig_child(int signo)
+{
+    puts("Got SIGCHLD signal, processing zombies...");
+
+    pid_t childpid;
+    while ((childpid = waitpid(-1, NULL, WNOHANG)) > 0)
+        printf("Child [%d] has been utilized\n", childpid);
+
+    if (childpid < 0 && errno != ECHILD)
+        err_quit("waitpid error");
 }
