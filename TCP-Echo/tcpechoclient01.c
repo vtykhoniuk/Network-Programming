213f9efca6e1e508b44a55ada7c5dc5b628ed55c
Volodymyr Tykhoniuk <volodymyr.tykhoniuk@gmail.com>
2016-04-09 00:27:10 -0400

    send stdin functionality has been added

--- TCP-Echo/tcpechoclient01.c
+++ TCP-Echo/tcpechoclient01.c
@@ -8,6 +8,8 @@
 // exit, free
 #include <stdlib.h>
 
+#include <stdio.h>
+
 // memset
 #include <string.h>
 
@@ -20,15 +22,25 @@ int main(int argc, char *argv[])
     if (argc != 2)
         err_quit("Usage: %s <IP>", argv[0]);
 
-    int sockfd = Socket(PF_INET, SOCK_STREAM, 0);
+    int sockfd = Socket(AF_INET, SOCK_STREAM, 0);
     struct sockaddr_in servaddr;
-    memset(&servaddr, 0, sizeof(struct sockaddr_in));
+    memset(&servaddr, 0, sizeof(servaddr));
 
     servaddr.sin_family = AF_INET;
     Sock_set_port((SA *) &servaddr, PORT);
-    Inet_pton(servaddr.sin_family, argv[1], &servaddr);
+    Sock_set_addr((SA *) &servaddr, argv[1]);
 
-    Connect(sockfd, (SA *) &servaddr, sizeof(struct sockaddr_in));
+    Connect(sockfd, (SA *) &servaddr, sizeof(servaddr));
+
+    char buf[MAXLINE];
+    while (fgets(buf, MAXLINE, stdin) != NULL) {
+        Writen(sockfd, buf, strlen(buf));
+        Readline(sockfd, buf, MAXLINE);
+        printf("%s", buf);
+    }
+
+    if (ferror(stdin))
+        err_sys("fgets error");
 
     Close(sockfd);
 
